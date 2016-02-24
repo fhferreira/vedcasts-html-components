1eab92d6a92508d6cfc762ac578a0074db9a289d
Flavio Ferreira <flaviometalvale@gmail.com>
2016-02-24 02:48:43 -0300

    05 - Criar o segundo componente

---
+++ components/cervejarias/card.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        props: ['dados']
+    }
+</script>
+<style>
+
+</style>
+<template>
+    <div class="well" >
+        <h3>{{ dados.name }}</h3>
+        <p>{{ dados.city }} / {{ dados.state }}</p>
+    </div>
+</template>
--- components/cervejarias/lista.vue
+++ components/cervejarias/lista.vue
@@ -1,9 +1,13 @@
 <script>
+    import VcCervejaria from './card.vue'
     export default {
         data(){
             return {
                 cervejarias: []
             }
+        },
+        components: {
+            VcCervejaria
         },
         ready() {
             this.$http.get('/cervejarias.json').then( (req) =>
@@ -14,7 +18,7 @@
 </script>
 <template>
     <h3>Componentes de cervejarias!</h3>
-    <div v-for="cervejaria in cervejarias">
-        <h3>{{ cervejaria.name }}</h3>
-    </div>
+    <vc-cervejaria
+            v-bind:dados="cervejaria"
+            v-for="cervejaria in cervejarias"></vc-cervejaria>
 </template>
