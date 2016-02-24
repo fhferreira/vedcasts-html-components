0765438fb2b63b440b80572720d924e895fd0626
Flavio Ferreira <flaviometalvale@gmail.com>
2016-02-24 02:19:03 -0300

    04 - Criar estrutura (HTML) e comportamento (JS)

--- components/cervejarias/lista.vue
+++ components/cervejarias/lista.vue
@@ -1,3 +1,20 @@
+<script>
+    export default {
+        data(){
+            return {
+                cervejarias: []
+            }
+        },
+        ready() {
+            this.$http.get('/cervejarias.json').then( (req) =>
+               this.cervejarias = req.data
+            );
+        }
+    }
+</script>
 <template>
     <h3>Componentes de cervejarias!</h3>
+    <div v-for="cervejaria in cervejarias">
+        <h3>{{ cervejaria.name }}</h3>
+    </div>
 </template>
