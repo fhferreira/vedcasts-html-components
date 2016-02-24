b5d75de16cdefc0601e7057d6e4f723d930bc352
Flavio Ferreira <flaviometalvale@gmail.com>
2016-02-24 02:02:19 -0300

    03 - Criação do Vue Object e primeiro componente

---
+++ components/cervejarias/lista.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>Componentes de cervejarias!</h3>
+</template>
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const Vue = require('vue')
 const Resource = require('vue-resource')
+
+const VcCervejarias = require('./components/cervejarias/lista.vue')
+
 Vue.use(Resource)
 
 new Vue({
     el: 'body',
-    data: {
-        label: 'Listar aqui! X'
+    components:{
+        'vc-cervejarias': VcCervejarias
     }
 })
